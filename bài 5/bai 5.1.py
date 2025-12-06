91733545306cb745f712a503ee1760bfe06b1a7d
muradshadow4-glitch <muradshadow.4@gmail.com>
2025-12-07 00:11:20 +0700

    Add files via upload

---
+++ bài 5/bai 5.1.py
@@ -0,0 +1,32 @@
+print("Hoang Nguyen Tuan Tu")
+print("245752021610129")
+print("--------------------")
+def square(n):   
+    return n * n
+
+def cube(n):
+    return n * n * n
+
+def average(values):
+    nvals = len(values)
+    total = 0.0
+    for v in values:
+        total += v
+    return float (total) / nvals
+# import module
+values = [2, 4, 6, 8, 10]
+
+print('Squares:')
+for v in values:
+    print(square(v))
+
+
+print('nCubes:')
+for v in values:
+    print(cube(v))
+
+print('Average:',average(values))
+
+print(square(2))
+print(square(3))
+
